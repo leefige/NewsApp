f1b927e6e52225470aadc767443c12e9374b8320
zhaojunhui <13687017969@163.com>
2017-09-14 01:49:41 +0800

    add share

--- app/src/main/java/com/java/group8/NewsPageActivity.java
+++ app/src/main/java/com/java/group8/NewsPageActivity.java
@@ -48,6 +48,8 @@ import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import cn.sharesdk.onekeyshare.OnekeyShare;
+
 import static com.java.group8.NewsPageActivity.mTts;
 
 /**
@@ -155,7 +157,14 @@ public class NewsPageActivity extends AppCompatActivity {
         mActionProvider.setContext(this);
 
         MenuItem share_newspage = menu.findItem(R.id.share_newspage);
-        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(share_newspage);
+        share_newspage.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                showShare();
+                return true;
+            }
+        });
+        //mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(share_newspage);
 //        MenuItem.OnMenuItemClickListener omicl = mActionProvider;
 //        read_newspage.setOnMenuItemClickListener(omicl);
         Intent sendIntent = new Intent();
@@ -242,6 +251,7 @@ public class NewsPageActivity extends AppCompatActivity {
                 else
                     Toast.makeText((NewsPageActivity)context, "取消收藏", Toast.LENGTH_SHORT).show();
             }
+
         }
         private void setSpanPart(SpannableString ss) {
             Pattern p;
@@ -287,6 +297,34 @@ public class NewsPageActivity extends AppCompatActivity {
         }
 
     }
+
+    private void showShare() {
+        cn.sharesdk.onekeyshare.OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+        // title标题，印象笔记、邮箱、信息、微信、人人网、QQ和QQ空间使用
+        oks.setTitle("标题");
+        // titleUrl是标题的网络链接，仅在Linked-in,QQ和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText(current_news.news_Title);
+        //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
+        oks.setImageUrl(current_news.news_URL);
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite("ShareSDK");
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+// 启动分享GUI
+        oks.show(this);
+    }
+
 }
 class MyReadActionProvider extends ActionProvider {
 
